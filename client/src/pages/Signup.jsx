dfec3877680d16617d3012504559824ad5d7fa6f
Zgibbs58 <zacharywgibbs@gmail.com>
2023-10-03 15:57:52 -0500

    Added signup error for not selecting a profile icon.

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -124,7 +124,7 @@ const Signup = () => {
                 </svg>
               </div>
               <div className="ml-3">
-                <h3 className="text-sm font-body text-white-800">Invalid login info</h3>
+                <h3 className="text-sm font-body text-white-800">Invalid signup info</h3>
                 <div className="mt-2 text-sm text-white-700">
                   <p>Please provide a unique email.</p>
                 </div>
@@ -145,7 +145,7 @@ const Signup = () => {
                 </svg>
               </div>
               <div className="ml-3">
-                <h3 className="text-sm font-body text-white-800">Invalid login info</h3>
+                <h3 className="text-sm font-body text-white-800">Invalid signup info</h3>
                 <div className="mt-2 text-sm text-white-700">
                   <p>Please provide a unique username.</p>
                 </div>
@@ -166,7 +166,7 @@ const Signup = () => {
                 </svg>
               </div>
               <div className="ml-3">
-                <h3 className="text-sm font-body text-white-800">Invalid login info</h3>
+                <h3 className="text-sm font-body text-white-800">Invalid signup info</h3>
                 <div className="mt-2 text-sm text-white-700">
                   <p>Please provide your location.</p>
                 </div>
@@ -187,10 +187,28 @@ const Signup = () => {
                 </svg>
               </div>
               <div className="ml-3">
-                <h3 className="text-sm font-body text-white-800">Invalid login info</h3>
+                <h3 className="text-sm font-body text-white-800">Invalid signup info</h3>
                 <div className="mt-2 text-sm text-white-700">
                   <p>Password must be at least 5 characters long.</p>
                 </div>
+              </div>
+            </div>
+          </div>
+        )}
+        {error && error.message.includes("icon") && (
+          <div className="m-8 rounded-md bg-black-700 shadow-[5px_2px_25px_5px_#6e91b8b6] p-4">
+            <div className="flex">
+              <div className="flex-shrink-0">
+                <svg className="h-5 w-5 text-red-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                  <path
+                    fillRule="evenodd"
+                    d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 2.495zM10 5a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 5zm0 9a1 1 0 100-2 1 1 0 000 2z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+              <div className="ml-3">
+                <h3 className="text-sm font-body text-white-800">Please select a profile icon.</h3>
               </div>
             </div>
           </div>
